3d9b0b01c1b0423d904d5df1bd6e79cd817c313c
rclemsmith <rclemsmith@gmail.com>
2020-08-02 21:15:23 +0530

    Search Change

--- Backend/routes/search.js
+++ Backend/routes/search.js
@@ -7,67 +7,82 @@ const Case = require("../models/cases");
 router.post("/:departmentName", (req, res) => {
   var query = req.body.query;
   console.log(query);
-  var tags = query.split(" ");
-  console.log(tags);
+  // var tags = query.split(" ");
+  // console.log(tags);
   var result = [];
+
+  // Case.find({ department: req.params.departmentName })
+  //   .then((cases) => {
+  //     cases.forEach((entry) => {
+  //       var count = 0;
+  //       tags.forEach((tag) => {
+  //         if(entry.synopsis){
+  //           if (entry.synopsis.toLowerCase().includes(tag.toLowerCase())) {
+  //             count++;
+  //           }
+  //         }
+
+  //       });
+  //       if (count == tags.length) {
+  //         result.push(entry);
+  //       }
+  //     });
+  //     console.log(result);
+  //     res.statusCode = 200;
+  //     res.json(result);
+  //   })
+  //   .catch((err) => console.log(err));
 
   Case.find({ department: req.params.departmentName })
     .then((cases) => {
-      cases.forEach((entry) => {
-        var count = 0;
-        tags.forEach((tag) => {
-          if(entry.synopsis){
-            if (entry.synopsis.toLowerCase().includes(tag.toLowerCase())) {
-              count++;
-            }
-          }
-          
-        });
-        if (count == tags.length) {
+      console.log(cases);
+
+      cases.forEach(function (entry) {
+        if (entry.type.toLowerCase().includes(query.toLowerCase())) {
+          var put = {
+            name: entry.name,
+            caseno : entry.caseno,
+            type: entry.type,
+            facts: entry.facts,
+            synopsis : entry.synopsis
+          };
+          result.push(entry);
+        } else if (entry.name.toLowerCase().includes(query.toLowerCase())) {
+          var put = {
+            name: entry.name,
+            type: entry.type,
+            caseno : entry.caseno,
+            facts: entry.facts,
+            synopsis : entry.synopsis
+          };
+          result.push(entry);
+        } else if (entry.facts.toLowerCase().includes(query.toLowerCase())) {
+          var put = {
+            name: entry.name,
+            type: entry.type,
+            caseno : entry.caseno,
+            facts: entry.facts,
+            synopsis : entry.synopsis
+          };
+          result.push(entry);
+        } else if (entry.synopsis.toLowerCase().includes(query.toLowerCase())) {
+          var put = {
+            name: entry.name,
+            type: entry.type,
+            caseno : entry.caseno,
+            facts: entry.facts,
+            synopsis : entry.synopsis
+          };
           result.push(entry);
         }
       });
-      console.log(result);
+
       res.statusCode = 200;
       res.json(result);
+
+      console.log(result);
     })
     .catch((err) => console.log(err));
-
-  // Case.find({department : req.params.departmentName})
-  //   .then((cases) => {
-  //     console.log(cases);
-
-  //     cases.forEach(function (entry) {
-  //       if (entry.type.toLowerCase().includes(query.toLowerCase())) {
-  //         var put = {
-  //           name: entry.name,
-  //           type: entry.type,
-  //           facts: entry.facts,
-  //         };
-  //         result.push(put);
-  //       } else if (entry.name.toLowerCase().includes(query.toLowerCase())) {
-  //         var put = {
-  //           name: entry.name,
-  //           type: entry.type,
-  //           facts: entry.facts,
-  //         };
-  //         result.push(put);
-  //       } else if (entry.facts.toLowerCase().includes(query.toLowerCase())) {
-  //         var put = {
-  //           name: entry.name,
-  //           type: entry.type,
-  //           facts: entry.facts,
-  //         };
-  //         result.push(put);
-  //       }
-  //     });
-
-  //     res.statusCode = 200;
-  //     res.json(result);
-
-  //     console.log(result);
-  //   })
-  //   .catch((err) => console.log(err));
 });
 
 router.post("/active/:departmentName", (req, res) => {
